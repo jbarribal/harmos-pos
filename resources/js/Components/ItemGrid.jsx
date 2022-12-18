15adf76dacce51babecfc0d3e56844380c8bedef
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-12-18 10:54:13 +0800

    create cart component in sales page

--- resources/js/Components/ItemGrid.jsx
+++ resources/js/Components/ItemGrid.jsx
@@ -3,7 +3,7 @@ import sample from '../../assets/hamburger.png'
 export default function ItemGrid({ items }) {
     return (
 
-        <div className='grid grid-cols-5 gap-4'>
+        <div className='grid grid-cols-3 gap-4'>
                 {items.map((item, index) => (
                     <div key={index} className="bg-white rounded-lg shadow-lg p-6 flex flex-col justify-between leading-normal">
                         <div>
