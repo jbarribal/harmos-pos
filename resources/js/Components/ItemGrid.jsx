cae4740a76ee23a5db2e61f94ca9516d2beb0b57
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-12-24 09:07:38 +0800

    add funciton to add items to cart when clicked

--- resources/js/Components/ItemGrid.jsx
+++ resources/js/Components/ItemGrid.jsx
@@ -1,19 +1,33 @@
 import sample from '../../assets/hamburger.png'
+import { useContext } from 'react';
+import { ThemeContext } from '@/Pages/Sales';
+
 
 export default function ItemGrid({ items }) {
-    return (
 
+
+    const addItem = useContext(ThemeContext)
+    return (
+        
+
+        
         <div className='grid grid-cols-3 gap-4'>
                 {items.map((item, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow-lg p-6 flex flex-col justify-between leading-normal">
+                    <button onClick={() => addItem(item)}> 
+                    <div key={index} className="bg-white hover:bg-gray-200 rounded-lg shadow-lg p-6 flex flex-col justify-between leading-normal hob">
                         <div>
                             <img className = 'h-32 justify-center m-auto'src={sample} alt='logo'/>
                         </div>
                         <div className = "text-center text-gray-900 font-bold text-xl mb-2 pt-2">
-                            {item}
+                            {item.name}
+                        </div>
+                        <div className = "text-center text-gray-900 text-s">
+                            ${item.price}
                         </div>
                     </div>
+                    </button>
                 ))}
+
         </div>
     );
 }
