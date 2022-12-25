9152a98bbe21c9dd5b51ba93199d89a11513d4bd
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-12-25 09:09:21 +0800

    Add dashboard cars to show total revenue, orders, online orders and walkin orders

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import DashboardCard from '../Components/DashboardCard';
 import { Head } from '@inertiajs/inertia-react';
 
 export default function Dashboard(props) {
@@ -14,6 +15,7 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">Welcome {props.auth.user.name}!</div>
+                        <DashboardCard />
 
                     </div>
                 </div>
--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,7 @@ export default defineConfig({
         }),
         react(),
     ],
+    commonjsOptions: {
+        esmExternals: true,
+     },
 });
