8dc53f41a49bb94dc80cd7e961247c39492d762d
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-12-27 11:46:40 +0800

    Modify Inventory Page
    Add Inventory Table
    Add Product Button
    Add Search Bar

--- resources/js/Pages/Inventory.jsx
+++ resources/js/Pages/Inventory.jsx
@@ -1,4 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import InventoryTable from '../Components/InventoryTable';
+import AddProductForm from '../Components/AddProductForm';
 import { Head } from '@inertiajs/inertia-react'
 import React from 'react'
 
@@ -14,7 +16,14 @@ export default function Inventory(props) {
     <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Inventory Page</div>
+                        <div>
+                          <input type="search" id="site-search" name='q' placeholder='Search'/>
+                        </div>
+                        <button className='border bg-gray-200'>
+                          Add Product
+                        </button>
+                        <AddProductForm />
+                        <InventoryTable />
                     </div>
                 </div>
             </div>
